4eb5a1eed125020061c09585f0af8fa535c20104
softwareNinja-888 <mlandvodlamini03@gmail.com>
2025-05-13 18:47:52 +0200

    Refactor gallery section

--- waypointMedia/src/components/Gallery.jsx
+++ waypointMedia/src/components/Gallery.jsx
@@ -10,7 +10,7 @@ function Card({img='/bg/11.avif',name="Coast Villa"}){
                 <div className="overflow-hidden aspect-w-1 aspect-h-1 h-90 w-full group-hover:rounded-2xl transition-all duration-500 ease-in-out">
                     <img className="object-cover w-full h-90 transition-all duration-300 hover:scale-125 cursor-pointer" src={img} alt={`${name} image`} loading='lazy' />
                 </div>
-                <div className="flex opacity-0 items-center justify-between border border-white bg-white py-2 text-xl font-Cal_Sans absolute bottom-5 w-11/12 h-20 rounded-xl px-5 mx-auto right-6 group-hover:opacity-100 transition-all duration-1000 ease-in-out">
+                <div className="flex lg:opacity-0 items-center justify-between border border-white bg-white py-2 text-xl font-Cal_Sans absolute bottom-5 w-11/12 h-20 rounded-xl px-5 mx-auto right-6 group-hover:opacity-100 transition-all duration-1000 ease-in-out">
                     <div>
                         {name}
                     </div>
@@ -30,7 +30,8 @@ export function Gallery() {
                 <div className="flex justify-center gap-2 text-6xl text-black text-center font-Cal_Sans uppercase">
                     Gallery                        
                   </div> 
-                <div className="grid grid-cols-2 mx-auto justify-center items-center w-10/12 gap-3">
+                <div className="grid grid-cols-1 md:grid-cols-2 mx-auto justify-center items-center w-11/12 lg:w-11/12 gap-18 md:gap-3">
+
                     <Card name="Coast Villa" img="/bg/11.avif"/>
                     <Card name="Peak House" img="/bg/12.avif"/>
                     <Card name="Oasis Residence" img="/bg/13.avif"/>
--- waypointMedia/src/components/WhyUs.jsx
+++ waypointMedia/src/components/WhyUs.jsx
@@ -1,32 +1,49 @@
 import { GoDotFill } from "react-icons/go";
 import { FaChartLine } from "react-icons/fa6";
+
+import { FaHandshake } from "react-icons/fa"
+import { AiFillCustomerService } from "react-icons/ai"
+import { FaTrophy } from "react-icons/fa"
+
 import { MdVisibility } from "react-icons/md";
 import { MdHotel } from "react-icons/md";
 import { PiStrategyBold } from "react-icons/pi";
 import { FaBook } from "react-icons/fa";
 
-function WhyCards({title,description="We take the time to deeply analyze your vision, industry, and target audience. Our goal is to craft a strategic foundation that aligns perfectly with your objectives, ensuring a strong start to the design process",icon=<FaChartLine size={13}/>}){
+function WhyCards({name,nameColor='text-blue-950',icon=<FaHandshake size={30}/>,iconColor='',description="We believe in open, honest communication and keep your best interests at the heart of everything",descriptionColor='',}){
 	return (
-		<>
-			<div className="flex flex-row gap-3 px-3 py-6 bg-blue-600/10 bg-center bg-cover bg-no-repeat cursor-pointer h-70 transition-all hover:scale-103 durstion-1000 ease-in" style={{backgroundImage:"url('/bg/shape1.avif')"}}>
-				<div className="bg-mainGreen p-2 rounded-full h-7 w-7">
-                    {icon}
-                </div>
-				<div className="flex flex-col gap-3">
-					<div className="text-2xl font-Cal_Sans h-18">
-						{title}
-					</div>
-					<div className="font-Poppins text-sm">{description}</div>
-				</div>
+		<div className={`flex flex-col gap-3 px-4 py-10 bg-white text-black rounded-2xl w-85 h-70`} >
+			<div className={`${iconColor}`}>
+				{icon}
 			</div>
-		</>
-		)
+			<div className={`text-2xl font-Cal_Sans ${nameColor}`}>{name}</div>
+			<div className={`text-lg font-Roboto`}>{description}</div>
+		</div>
+	)
 }
+
+// function WhyCards({title,description="We take the time to deeply analyze your vision, industry, and target audience. Our goal is to craft a strategic foundation that aligns perfectly with your objectives, ensuring a strong start to the design process",icon=<FaChartLine size={13}/>}){
+// 	return (
+// 		<>
+// 			<div className="flex flex-row gap-3 px-3 py-6 bg-blue-600/10 bg-center bg-cover bg-no-repeat cursor-pointer h-70 transition-all hover:scale-103 durstion-1000 ease-in" style={{backgroundImage:"url('/bg/shape1.avif')"}}>
+// 				<div className="bg-mainGreen p-2 rounded-full h-7 w-7">
+//                     {icon}
+//                 </div>
+// 				<div className="flex flex-col gap-3">
+// 					<div className="text-2xl font-Cal_Sans h-18">
+// 						{title}
+// 					</div>
+// 					<div className="font-Poppins text-sm">{description}</div>
+// 				</div>
+// 			</div>
+// 		</>
+// 		)
+// }
 export function WhyUs({content}){
 	return (
 			<>
 				<div className=" bg-center bg-cover bg-no-repeat py-40" style={{backgroundImage:"url('/bg/shape2.avif')"}}>
-					<div className="flex w-11/12 mx-auto items-center gap-20 py-20 ">
+					<div className="flex flex-col md:flex-row w-11/12 mx-auto items-center gap-20 py-20 ">
 						<div className=" text-5xl font-Cal_Sans flex-1/3">
 							Why Choose <span className="bg-mainGreen">{content.name},</span> for your Marketing Efforts?
 						</div>
@@ -34,10 +51,10 @@ export function WhyUs({content}){
 							<img src="/bg/16.avif" alt="" className="h-[500px] w-full"/>
 						</div>
 					</div>
-					<div className="flex flex-row w-10/12 mx-auto gap-10">
-						<WhyCards title="100% Satisfaction Guarantee" />
-						<WhyCards title="Niche Tourism Expertise" />
-						<WhyCards title="Viral Content Creation" />
+					<div className="grid md:grid-cols-2 lg:grid-cols-3 w-10/12 md:w-11/12 mx-auto gap-10">
+						<WhyCards name='Niche expertise in hospitality'/>
+						<WhyCards name='Content tailored for tourism' icon={<AiFillCustomerService size={30}/>}/>
+						<WhyCards name='Proven Growth Strategies' icon={<FaTrophy size={30}/>}/>
 					</div>
 				</div>
 			</>
